e7f64ded77f07de3faade6adc94ba43b81d9eb43
Lubna <muspaksamsu@gmail.com>
2024-07-14 13:07:55 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,12 @@
+// Start with a copy of your program from Exercise 39. Write a function called make_great() that modifies the array of magicians by adding the phrase the Great to each magician’s name. Call show_magicians() to see that the list has actually been modified.
+function show_magicians(magicians : string[]){
+    magicians.forEach(name => console.log(name)
+);
+}
+function make_great(magicians: string[]){
+    return magicians.map(name => `The Great ${name}`)
+}
+let magicians_names =["Uzma" , "Alia" , "saba"]
+let great_magicians = make_great(magicians_names)
+console.log(magicians_names);
+
